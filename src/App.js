efb05513d6608e296ca5a85f7a1385204a75e5e8
Firas Al-Houssari <f.alhusari@gmail.com>
2019-11-19 08:18:58 +0200

    Added firebase facebook auth

--- src/App.js
+++ src/App.js
@@ -1,36 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Facebook from "./components/Facebook/Facebook";
 import ShopList from "./components/ShopList/ShopList";
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { QueryParamProvider } from "use-query-params";
-import logo from "./logo.svg";
+import firebase from 'firebase';
+import {StyledFirebaseAuth} from 'react-firebaseui'
 import "./App.css";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <h2>Welcome to Doner Stars</h2>
-//         <p>Join the Ranking of the best Doners in Sofia</p>
-//         <Facebook />
-//         <ShopList />
-//         {/* <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p> */}
-//       </header>
-//     </div>
-//   );
-// }
+// firebase.initializeApp({
+//   apiKey: "AIzaSyARpNNPyNbkTO3EUuKK_4E8PuHDuqjIJog",
+//   authDomain: "doner-stars.firebaseapp.com"
+// })
 
 const App = () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  }
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => {
+      setIsSignedIn(!!user);
+      console.log("user", !!user);
+    })
+  })
+
   return (
-    <QueryParamProvider ReactRouterRoute={Route}>
-      <Switch>
-        <Route component={Facebook} exact path="/login" />
-        
-      </Switch>
-    </QueryParamProvider>
+    <div className="App">
+      {isSignedIn ? (
+      <div>
+        <div>Singed In!</div>
+        <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
+        <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+        <img alt="profile_pic" src={firebase.auth().currentUser.photoURL} />
+      </div>
+      ) : ( 
+      <StyledFirebaseAuth 
+      uiConfig={uiConfig}
+      firebaseAuth={firebase.auth()}/>
+      )}
+    </div>
   );
 }
+
+// const App = () => {
+//   return (
+//     <QueryParamProvider ReactRouterRoute={Route}>
+//       <Switch>
+//         <Route component={Facebook} exact path="/login" />
+        
+//       </Switch>
+//     </QueryParamProvider>
+//   );
+// }
 
 export default App;
