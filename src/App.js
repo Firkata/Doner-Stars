f88a2fa922470a540a5e44784da2bc0a0e0c64a1
Firas Al-Houssari <firas.al-houssari@tailify.com>
2019-11-22 18:21:45 +0200

    added tabler css

--- src/App.js
+++ src/App.js
@@ -1,59 +1,59 @@
 import React, { useState, useEffect } from "react";
 import Facebook from "./components/Facebook/Facebook";
 import ShopList from "./components/ShopList/ShopList";
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch, Redirect } from "react-router-dom";
 import { QueryParamProvider } from "use-query-params";
-import firebase from 'firebase';
-import {StyledFirebaseAuth} from 'react-firebaseui'
+import firebase from "firebase";
+import { StyledFirebaseAuth } from "react-firebaseui";
+import "tabler-react/dist/Tabler.css";
 import "./App.css";
 
 const App = () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
   const uiConfig = {
     signInFlow: "popup",
-    signInOptions: [
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID
-    ],
+    signInOptions: [firebase.auth.FacebookAuthProvider.PROVIDER_ID],
     callbacks: {
       signInSuccess: () => false
     }
-  }
+  };
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged(user => {
       setIsSignedIn(!!user);
       // console.log("user", !!user);
-    })
-  })
+    });
+  });
 
   return (
     <div className="App">
       {isSignedIn ? (
-      <div>
-        <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
-        <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
-        <img alt="profile_pic" src={firebase.auth().currentUser.photoURL} />
-        <ShopList />
-      </div>
-      ) : ( 
-      <header className="App-header">
-        <h2>Welcome to Doner Stars</h2>
-        <p>Join the Ranking of the best Doners in Sofia</p>
-        <StyledFirebaseAuth 
-        uiConfig={uiConfig}
-        firebaseAuth={firebase.auth()}/>
-      </header>
+        <div>
+          <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
+          <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+          <img alt="profile_pic" src={firebase.auth().currentUser.photoURL} />
+          <ShopList />
+        </div>
+      ) : (
+        <header className="App-header">
+          <h2>Welcome to Doner Stars</h2>
+          <p>Join the Ranking of the best Doners in Sofia</p>
+          <StyledFirebaseAuth
+            uiConfig={uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+        </header>
       )}
     </div>
   );
-}
+};
 
 // const App = () => {
 //   return (
 //     <QueryParamProvider ReactRouterRoute={Route}>
 //       <Switch>
 //         <Route component={Facebook} exact path="/login" />
-        
+
 //       </Switch>
 //     </QueryParamProvider>
 //   );
--- src/components/ShopCard/ShopCard.js
+++ src/components/ShopCard/ShopCard.js
@@ -1,21 +1,33 @@
-import React from 'react';
-import { Grid, Card, Button } from 'tabler-react';
-import styles from './ShopCard.module.css';
+import React from "react";
+import { Grid, Card, Button } from "tabler-react";
+import styles from "./ShopCard.module.css";
 
-const ShopCard = (data) => {
-    return (
-        <Card className={styles.CardContainer}>
-            <p>{data.name}</p>
-            <p>{data.location}</p>
-            <p>{data.rating}</p>
-            <p>{data.submittedRatings}</p>
-            <Grid.Row>
-                <Grid.Col lg="2"><button>1</button></Grid.Col>
-                <Grid.Col lg="2"><button>2</button></Grid.Col>
-                <Grid.Col lg="2"><button>3</button></Grid.Col>
-            </Grid.Row>
-        </Card>
-    );
-}
+const ShopCard = data => {
+  return (
+    <Card className={styles.CardContainer}>
+      <p>{data.name}</p>
+      <p>{data.location}</p>
+      <p>{data.rating}</p>
+      <p>{data.submittedRatings}</p>
+      <Grid.Row className={styles.RatingRow}>
+        <Grid.Col lg="2.5">
+          <button>1</button>
+        </Grid.Col>
+        <Grid.Col lg="2.5">
+          <button>2</button>
+        </Grid.Col>
+        <Grid.Col lg="2.5">
+          <button>3</button>
+        </Grid.Col>
+        <Grid.Col lg="2.5">
+          <button>4</button>
+        </Grid.Col>
+        <Grid.Col lg="2.5">
+          <button>5</button>
+        </Grid.Col>
+      </Grid.Row>
+    </Card>
+  );
+};
 
 export default ShopCard;
